dd5aea971222bb2b9647ad9e528ca1c469861bfb
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-29 14:14:13 -0700

    Refactors api call out to facade design pattern

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,18 +1,10 @@
 class DashboardController < ApplicationController
 
   def show
-    conn = Faraday.new(
-      url: 'https://api.github.com',
-      headers: {
-        'Authorization': "token #{current_user.token}"
-      }
-    )
-    response = conn.get('/user/repos')
-    # make Repo record and associate to user, user must be created with
-    # repos = nil
+    repos = RepoFacade.get_repos(current_user)
 
-    # How this is set up the response will fetch the data regardless, so does
-    # it really need to be stored in the DB? Use a PORO?
-    @user_repos = JSON.parse(response.body, symbolize_names: true)
+    @repo_list = repos.map do |repo|
+      Repo.new(repo)
+    end
   end
 end
