63cd4d496d4f0f0ac487ed0b3e60635ae91401e7
Shaun James <ShaunDaneJames@Gmail.com>
2020-12-29 13:27:28 -0700

    Creates dashboard controller with show method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,18 @@
 class DashboardController < ApplicationController
 
-  def show; end 
+  def show
+    conn = Faraday.new(
+      url: 'https://api.github.com',
+      headers: {
+        'Authorization': "token #{current_user.token}"
+      }
+    )
+    response = conn.get('/user/repos')
+    # make Repo record and associate to user, user must be created with
+    # repos = nil
+
+    # How this is set up the response will fetch the data regardless, so does
+    # it really need to be stored in the DB? Use a PORO?
+    @user_repos = JSON.parse(response.body, symbolize_names: true)
+  end
 end
